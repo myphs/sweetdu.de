6cf5d4dab604d8e7e162ba6b0ab0aeb25ebe9346
myphs <myphs+github@posteo.de>
2019-07-22 10:11:58 +0200

    initial commit

---
+++ I_COULD_SHOW_YOU_MY_CODE.BUT_AM_I_GONNA.js
@@ -0,0 +1,23 @@
+/* yeah, you're not supposed to be here, do you know that?
+   like, seriously, go away! */	
+var yes_i_cant_find_the_original_source_anymore = true;
+var and_now_i_have_to_make_all_changes_like_this = window.requestAnimationFrame;
+var is_this_self_explanotary_code_questionmark = document.getElementById("YES_THIS_CODE_IS_REALLY_GUD");
+const oh_boy_dont_forget_to_use_constants_so_your_code_works_better = " P A U S E &nbsp; A N I M A T I O N"; // this line is out of use, but don't forget to keep old code, so you can have the chills of nostalgia
+jsInHrefsSoHotBackThenInThe90s = () => {
+	if (yes_i_cant_find_the_original_source_anymore) {
+		and_now_i_have_to_make_all_changes_like_this = (argument="argumentn't") => {};
+		yes_i_cant_find_the_original_source_anymore = false;
+		is_this_self_explanotary_code_questionmark.classList.add("PAUSENT");
+	} else {
+		and_now_i_have_to_make_all_changes_like_this = window.requestAnimationFrame;
+		yes_i_cant_find_the_original_source_anymore = true;
+		is_this_self_explanotary_code_questionmark.classList.remove("PAUSENT");
+		window.requestAnimationFrame(u);
+	}
+};
+for(var b="11110100010111101111011111l1zero123415167891000000100l11110101010111101111000100l00010101010100001000000100l11110111110111101111000100l IS THIS BINARY DATA ???? l YES OF COURSE YOU DUMMIE l11110010001LOL021111001111l1NOO101ITS1terna1ry0101000l10001010001ZERO01000101111l100010100010ZERO1000101000l11110001110001001111001111".split("l"),c=0;c<b.length;c++)b[c]=b[c].split("").map(function(a){return"1"===a});var d=document.getElementById("can"),e=d.getContext("2d");e.translate(.5,.5);e.b=!1;
+var g=[],h=window.innerHeight,k=window.innerWidth,l=[];function n(){for(var a=1;h>b.length&&k>b[0].length;)a*=2,d.height=h=window.innerHeight/a|0,d.width=k=window.innerWidth/a|0;d.height=h*=4;d.width=k*=4;a=h/2-b.length/2*2|0;var f=k/2-b[0].length/2*2|0;l=[];for(var m=0;m<b.length;m++)for(var p=0;p<b[m].length;p++)b[m][p]&&l.push({x:f+2*p,y:a+2*m});if(!yes_i_cant_find_the_original_source_anymore){jsInHrefsSoHotBackThenInThe90s();jsInHrefsSoHotBackThenInThe90s();}}n();window.addEventListener("resize",n);var q,r,t;
+function u(){e.clearRect(0,0,k,h);for(var a=0;a<g.length;a++){q=(99991*g[a].x|0)%k;r=h-g[a].y|0;t=!1;g[a].y+=g[a].speed;for(var f=0;f<l.length;f++)if((q|0)>=l[f].x&&(q|0)<l[f].x+2&&r>=l[f].y&&r<l[f].y+2){t=!0;break}t?(g[a].a=Math.min(255,5+g[a].a),g[a].speed=.01):(g[a].a=Math.max(g[a].a-=5,0),g[a].speed+=g[a].x/128);g[a].y-2>h&&(g[a]=v());e.fillStyle="rgba("+g[a].a+",0,158,"+(.3+.5*g[a].x-g[a].y/h*.6+g[a].a/255)+")";e.fillRect(q,r|0,1,1);}and_now_i_have_to_make_all_changes_like_this(u)}and_now_i_have_to_make_all_changes_like_this(u);
+function v(){return{x:Math.random(),y:0,speed:Math.random()/16,a:0}}function w(){g.push(v());g.push(v());g.push(v());g.push(v());g.push(v());g.push(v());g.push(v());g.push(v());g.push(v());g.push(v());4096>g.length&&setTimeout(w,128)}w();/*d.addEventListener("mousedown",function(){try{d.mozRequestFullScreen()}catch(a){}try{d.webkitRequestFullscreen()}catch(a){}try{d.requestFullscreen()}catch(a){}});*/document.addEventListener('contextmenu', e => e.preventDefault());
+/*don't do this. this is BAD practice. instead use one huge image which is loaded once and drag the background around in css. this is just for the lulz. trust me. or not. who knows. don't trust anyone. not even yourself. who are you anyway? ok, now the comment is long enough so probably noone sees this garbage code. nice.*/for(var img of ["a","b","c","d","hahahaha","letter_after_d","letter_after_letter_after_d","second_letter_after_letter_after_d","CoNtAkkkT","c0nTaKKKt"]) {document.createElement("img").setAttribute("src", "./pls_dont_take_a_look_inside_ok_thank_you/"+img+".png");}
